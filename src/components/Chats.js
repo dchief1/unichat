a6731daf47bf0133a23b2a546ff9db2c7ac08968
dchief1 <igwesiemmanuel4@gmail.com>
2021-09-01 23:56:15 +0100

    check

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -46,7 +46,7 @@ const Chats = () => {
         .catch(() => {
             let formdata = new FormData()
             formdata.append('email', user.email)
-            formdata.append('username', user.email)
+            formdata.append('username', user.displayName)
             formdata.append('secret', user.uid)
 
             getFile(user.photoURL)
